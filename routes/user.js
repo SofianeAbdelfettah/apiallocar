1b9b2d6eac53ef5ce3b61ca7070b683442761616
SofianeAbdelfettah <abdelf_s@etna-alternance.net>
2017-06-20 12:51:52 +0200

    first commit

---
+++ routes/user.js
@@ -0,0 +1,56 @@
+var models = require('../sequelize/models/'); // loads index.js
+var User = models.Nom; // the model keyed by its name
+
+module.exports = {
+  //Get a list of all authors using model.findAll()
+  index(req, res) {
+    User.findAll().then(function(users) {
+        res.status(200).json(users);
+      })
+      .catch(function(error) {
+        res.status(500).json(error);
+      });
+  },
+  add(req, res) {
+
+
+    User.findAll({
+      where: {
+        name: req.body.name
+      }
+    }).then(function(users) {
+      if (users.length === 0) {
+        User.create(req.body.name)
+          .then(function(newUser) {
+            return res.status(200).json(newUser);
+          })
+          .catch(function(error) {
+            return res.status(500).json(error);
+          });
+      } else return res.send("user already exist")
+
+    }).catch(function(error) {
+      return res.json(error);
+    });
+
+
+
+
+  },
+  one(req, res) {
+    User.findAll({
+      where: {
+        name: req.params.name
+      }
+    }).then(function(users) {
+      if (users.length === 0) {
+        return res.send('no user found');
+      }
+      return res.json(users);
+    }).catch(function(error) {
+      res.json(error);
+    });
+
+
+  }
+}
---
+++ server.js
@@ -0,0 +1,39 @@
+const compression = require('compression');
+const bodyParser = require('body-parser')
+const base64Img = require('base64-img');
+const express = require('express');
+const app = require('express')();
+users = require('./routes/user.js');
+
+
+  app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+  });
+
+  app.use(bodyParser.urlencoded({
+      extended: true
+  }));
+
+app.use(express.static('public'));
+app.use(compression());
+
+
+app.get('/users', users.index);
+app.post('/add', users.add);
+app.get('/one/:name', users.one);
+
+app.post('/', function (req, res) {
+  if (req.body.picture != "" && req.body.name != "") {
+    base64Img.img(req.body.picture, 'public/pictures/'+req.body.name, '1', function(err, filepath) {});
+    res.send('ok');
+  }
+});
+
+app.set('port', process.env.POR || 8000);
+app.listen(app.get('port'), function () {
+  console.log("Magic happens on port", app.get('port'));
+});
